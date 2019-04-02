24d1198596581b9d4ee70877094507a8630f189a
Henri Buyse <henri.buyse@gmail.com>
2019-04-02 12:07:54 +0200

    Flake8

--- mult_mixins/mixins.py
+++ mult_mixins/mixins.py
@@ -18,7 +18,7 @@ class StaffMixin(UserPassesTestMixin):
     """Mixin allows you to require a user with `is_staff` set to True."""
 
     raise_exception = True
-    permission_denied_message = _("You are not a staff user. You do not have the right to view this page.")            # from AccessMixin
+    permission_denied_message = _("You are not a staff user. You do not have the right to view this page.")
 
     def test_func(self):
         """Check if the logged user is staff."""
@@ -29,7 +29,7 @@ class SuperuserMixin(UserPassesTestMixin):
     """Mixin allows you to require a user with `is_superuser` set to True."""
 
     raise_exception = True
-    permission_denied_message = _("You are not a superuser user. You do not have the right to view this page.")            # from AccessMixin
+    permission_denied_message = _("You are not a superuser user. You do not have the right to view this page.")
 
     def test_func(self):
         """Check if the logged user is superuser."""
@@ -39,11 +39,12 @@ class SuperuserMixin(UserPassesTestMixin):
 class OwnerMixin(UserPassesTestMixin):
     """Mixin that test if the logged user owns the page."""
 
-    permission_denied_message = _("You are not the owner of the page. You cannot view it.")            # from AccessMixin
+    permission_denied_message = _("You are not the owner of the page. You cannot view it.")
     raise_exception = True
     owner_kwargs = None
 
     def get_owner_kwargs(self):
+        """Retrieve the owner_kwargs if given otherwise it returns 'username'."""
         if self.owner_kwargs is None:
             return 'username'
         return self.owner_kwargs
@@ -61,11 +62,12 @@ class OwnerMixin(UserPassesTestMixin):
 class OwnerOrStaffMixin(UserPassesTestMixin):
     """Mixin that check if the user logged in has the rights to view the page."""
 
-    permission_denied_message = _("You are not the owner of the page nor a staff user. You cannot view it.")            # from AccessMixin
+    permission_denied_message = _("You are not the owner of the page nor a staff user. You cannot view it.")
     raise_exception = True
     owner_kwargs = None
 
     def get_owner_kwargs(self):
+        """Retrieve the owner_kwargs if given otherwise it returns 'username'."""
         if self.owner_kwargs is None:
             return 'username'
         return self.owner_kwargs
